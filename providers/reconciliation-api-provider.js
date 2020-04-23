4c06338c0d9012c18ea50d64707c7ba5d6343587
Stefan Peters <stefandesu@exo.pm>
2020-04-23 12:14:25 +0200

    Adjust ReconciliationApiProvider to new BaseProvider

--- providers/reconciliation-api-provider.js
+++ providers/reconciliation-api-provider.js
@@ -3,20 +3,17 @@ const jskos = require("jskos-tools")
 const _ = require("lodash")
 const qs = require("qs")
 
-// TODO!!!
-
 /**
  * Provider for the OpenRefine Reconciliation API.
  */
 class ReconciliationApiProvider extends BaseProvider {
 
-  constructor(...params) {
-    super(...params)
+  _setup() {
     this.has.mappings = true
-    this.cache = []
+    this._cache = []
   }
 
-  _getMappings({ from, to, mode }) {
+  async getMappings({ from, to, mode, ...config }) {
     let swap
     let concept
     let fromConceptScheme = _.get(from, "inScheme[0]")
@@ -35,18 +32,18 @@ class ReconciliationApiProvider extends BaseProvider {
       toScheme = (this.registry.schemes || []).find(scheme => jskos.compare(scheme, _.get(from, "inScheme[0]"))) || (this.registry.schemes || [])[0]
     }
     // Temporary to filter out GND mapping requests...
-    // FIXME: Remove!
+    // TODO: Remove?!?
     if (mode != "or" || !concept || !this.registry.reconcile) {
-      return Promise.resolve([])
+      return []
     }
     // If concept's scheme is the same as reconciliation scheme, skip
     if (!fromScheme || !toScheme || jskos.compare(fromScheme, toScheme)) {
-      return Promise.resolve([])
+      return []
     }
     // Prepare labels
     let language = jskos.languagePreference.selectLanguage(concept.prefLabel)
     if (!language) {
-      return Promise.resolve([])
+      return []
     }
     let altLabels = _.get(concept, `altLabel.${language}`, [])
     if (_.isString(altLabels)) {
@@ -56,56 +53,55 @@ class ReconciliationApiProvider extends BaseProvider {
     let labels = altLabels.concat([prefLabel])
     labels = [prefLabel]
     // Get results from API or cache
-    return this.__getReconciliationResults(labels, language).then(({ url, data: results }) => {
-      results = [].concat(...Object.values(results).map(value => value.result))
-      // Sort results, first by score descending, then by match, then by length of notation
-      results = results.sort((a, b) => {
-        if (a.score != b.score) {
-          return b.score - a.score
-        }
-        if (a.match != b.match) {
-          if (a.match) {
-            return -1
-          } else {
-            return 1
-          }
-        }
-        return a.id.length - b.id.length
-      })
-      // Prepare namespace
-      let namespace = _.get(toScheme, "namespace", "")
-      // Map results to actual mappings
-      let mappings = results.map(result => ({
-        fromScheme,
-        from: { memberSet: [concept] },
-        toScheme,
-        to: { memberSet: [
-          {
-            uri: namespace + result.id,
-          },
-        ] },
-        type: [
-          result.match ?
-            "http://www.w3.org/2004/02/skos/core#exactMatch" :
-            (
-              result.score >= 80 ?
-                "http://www.w3.org/2004/02/skos/core#closeMatch" :
-                "http://www.w3.org/2004/02/skos/core#mappingRelation"
-            ),
-        ],
-      }))
-      if (swap) {
-        // Swap mapping sides if only `to` was set
-        mappings = mappings.map(mapping => Object.assign(mapping, {
-          fromScheme: mapping.toScheme,
-          from: mapping.to,
-          toScheme: mapping.fromScheme,
-          to: mapping.from,
-        }))
+    let { url, data: results } = await this._getReconciliationResults({ ...config, labels, language })
+    results = [].concat(...Object.values(results).map(value => value.result))
+    // Sort results, first by score descending, then by match, then by length of notation
+    results = results.sort((a, b) => {
+      if (a.score != b.score) {
+        return b.score - a.score
       }
-      mappings.url = url
-      return mappings
+      if (a.match != b.match) {
+        if (a.match) {
+          return -1
+        } else {
+          return 1
+        }
+      }
+      return a.id.length - b.id.length
     })
+    // Prepare namespace
+    let namespace = _.get(toScheme, "namespace", "")
+    // Map results to actual mappings
+    let mappings = results.map(result => ({
+      fromScheme,
+      from: { memberSet: [concept] },
+      toScheme,
+      to: { memberSet: [
+        {
+          uri: namespace + result.id,
+        },
+      ] },
+      type: [
+        result.match ?
+          "http://www.w3.org/2004/02/skos/core#exactMatch" :
+          (
+            result.score >= 80 ?
+              "http://www.w3.org/2004/02/skos/core#closeMatch" :
+              "http://www.w3.org/2004/02/skos/core#mappingRelation"
+          ),
+      ],
+    }))
+    if (swap) {
+      // Swap mapping sides if only `to` was set
+      mappings = mappings.map(mapping => Object.assign(mapping, {
+        fromScheme: mapping.toScheme,
+        from: mapping.to,
+        toScheme: mapping.fromScheme,
+        to: mapping.from,
+      }))
+    }
+    mappings.url = url
+    return mappings
   }
 
   /**
@@ -113,14 +109,14 @@ class ReconciliationApiProvider extends BaseProvider {
    *
    * @param {*} labels - list of labels
    */
-  __getReconciliationResults(labels, language) {
+  async _getReconciliationResults({ labels, language, ...config }) {
     labels = labels.sort()
     // Use local cache.
-    let resultsFromCache = this.cache.find(item => {
+    let resultsFromCache = this._cache.find(item => {
       return _.isEqual(item.labels, labels) && item.language == language
     })
     if (resultsFromCache) {
-      return Promise.resolve(resultsFromCache)
+      return resultsFromCache
     }
     // Prepare queries
     let queries = {}
@@ -131,29 +127,31 @@ class ReconciliationApiProvider extends BaseProvider {
       }
       index += 1
     }
-    let url =this.registry.reconcile
+    let url = this.registry.reconcile
     if (language) {
       url = url.replace("{language}", language)
     }
     queries = JSON.stringify(queries)
-    if (url.startsWith("http:") && typeof window !== "undefined" && window.location.protocol == "https:") {
-      return Promise.resolve({})
-    }
-    return this.post(url, qs.stringify({ queries }), {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    }).then(data => {
-      data = data || {}
-      let newCacheEntry = {
-        labels,
-        language,
-        data,
-        url: `${url}${url.includes("?") ? "&" : "?"}${qs.stringify({ queries })}`,
-      }
-      this.cache.push(newCacheEntry)
-      return newCacheEntry
+    // Set appropriate header
+    _.set(config, ["headers", "Content-Type"], "application/x-www-form-urlencoded")
+    let data = await this.axios({
+      ...config,
+      url,
+      data: qs.stringify({ queries }),
     })
+    data = data || {}
+    let newCacheEntry = {
+      labels,
+      language,
+      data,
+      url: `${url}${url.includes("?") ? "&" : "?"}${qs.stringify({ queries })}`,
+    }
+    this._cache.push(newCacheEntry)
+    // Make sure there are a maximum of 20 entries in cache
+    if (this._cache.length > 20) {
+      this._cache = this._cache.slice(this._cache.length - 20)
+    }
+    return newCacheEntry
   }
 
 }
