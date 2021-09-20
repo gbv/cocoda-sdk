9d35c984e74e895a0a87196ad460b4dc6629b298
Stefan Peters <stefandesu@exo.pm>
2021-09-20 09:31:28 +0300

    Use lodash-es instead of lodash to utilize tree shaking

--- src/utils/lodash.js
+++ src/utils/lodash.js
@@ -1,21 +1,18 @@
 // Exports a subset of lodash methods
-
-import lodash from "lodash"
-
-export const get = lodash.get
-export const set = lodash.set
-export const uniq = lodash.uniq
-export const intersection = lodash.intersection
-export const union = lodash.union
-export const forOwn = lodash.forOwn
-// TOOD: Use Array.isArray instead
-export const isArray = lodash.isArray
-export const isObject = lodash.isObject
-export const isString = lodash.isString
-export const isEmpty = lodash.isEmpty
-export const isEqual = lodash.isEqual
-export const merge = lodash.merge
-export const last = lodash.last
-export const omit = lodash.omit
-// TODO: Use native concat instead
-export const concat = lodash.concat
+export {
+  get,
+  set,
+  uniq,
+  intersection,
+  union,
+  forOwn,
+  isArray,
+  isObject,
+  isString,
+  isEmpty,
+  isEqual,
+  merge,
+  last,
+  omit,
+  concat,
+} from "lodash-es"
