5a6614ea3c7dd7d731b30b740db45290f205d4f7
Stefan Peters <Stefan.Peters@gbv.de>
2023-05-08 14:12:44 +0200

    Add example for LobidApi

---
+++ examples/lobid.js
@@ -0,0 +1,40 @@
+// This is all the setup that's needed!
+import { cdk, addAllProviders } from "../src/index.js"
+addAllProviders()
+const registry = cdk.initializeRegistry({
+  provider: "ConceptApi",
+  api: "https://bartoc.org/api/",
+});
+
+// Run rest of commands asynchronously
+(async () => {
+  console.log("Loading schemes from BARTOC API... (may take a while when loading many schemes)")
+  const schemes = await registry.getSchemes({
+    params: {
+      uri: "http://bartoc.org/en/node/430",
+    },
+  })
+  console.log(`Loaded ${schemes.length} schemes.`)
+
+  for (let scheme of schemes) {
+    if (!scheme._registry || scheme._registry === registry) {
+      // Fallback by default is the registry that provided the scheme (BARTOC in this case), but we don't want that.
+      continue
+    }
+    console.log()
+    const id = scheme.notation ? scheme.notation[0] : scheme.uri
+    const schemeRegistry = scheme._registry // registry by which scheme can be accessed
+    console.log(`Found scheme ${id}, it was initialized with ${schemeRegistry._jskos.provider} (${schemeRegistry._jskos.api})`)
+    console.log(`Loading top concepts for ${id}...`)
+    try {
+      const top = await scheme._getTop() // or = await schemeRegistry.getTop({scheme})
+      console.log(`Loaded ${top.length} top concepts.`)
+      const search = "Europ"
+      const concepts = await schemeRegistry.search({scheme, search, limit: 3})
+      console.log(`Sample search for ${search} resulted in:`)
+      console.log(concepts.map(({uri,prefLabel}) => ({uri,prefLabel})))
+    } catch (error) {
+      console.error("Error loading top concepts:", error)
+    }
+  }
+})()
