9e5dfc547258e538cfa7fa6e21f254e3c63cddff
Arno <arno.kesper@uni-marburg.de>
2026-04-30 13:30:38 +0200

    Consider paginated results for TopConcepts, Narrower and Ancestors

--- src/providers/ols-api-provider.js
+++ src/providers/ols-api-provider.js
@@ -312,11 +312,8 @@ export default class OlsApiProvider extends BaseProvider {
   async getTop({ scheme }) {
     const VOCID = await this._getSchemeVOCID(scheme)
     if (VOCID) {
-      let url = this._getApiUrl(["ontologies", VOCID, "classes"], { hasDirectParents: "false" })
-      let response = await this._request(url)
-      if (response?.elements) {
-        return Promise.all(response.elements.map(item => this._termToJSKOS(item)))
-      }
+      let response = await this._paginate(["ontologies", VOCID, "classes"], { hasDirectParents: "false" }, null)
+      return Promise.all(response.map(item => this._termToJSKOS(item)))
     }
     return []
   }
@@ -334,11 +331,8 @@ export default class OlsApiProvider extends BaseProvider {
   async getNarrower({ concept }) {
     const { VOCID, iri } = await this._splitConcept(concept)
     if (VOCID && iri) {
-      let url = this._getApiUrl(["ontologies", VOCID, "classes", iri, "children"])
-      let response = await this._request(url)
-      if (response?.elements) {
-        return Promise.all(response.elements.map(item => this._termToJSKOS(item)))
-      }
+      const items = await this._paginate(["ontologies", VOCID, "classes", iri, "children"], {}, 0)
+      return Promise.all(items.map(item => this._termToJSKOS(item)))
     }
     return []
   }
@@ -346,11 +340,8 @@ export default class OlsApiProvider extends BaseProvider {
   async getAncestors({ concept }) {
     const { VOCID, iri } = await this._splitConcept(concept)
     if (VOCID && iri) {
-      let url = this._getApiUrl(["ontologies", VOCID, "classes", iri, "ancestors"])
-      let response = await this._request(url)
-      if (response?.elements) {
-        return Promise.all(response.elements.map(item => this._termToJSKOS(item)))
-      }
+      let response = await this._paginate(["ontologies", VOCID, "classes", iri, "ancestors"], {}, null)
+      return Promise.all(response.map(item => this._termToJSKOS(item)))
     }
     return []
   }
